618b36425b32460f939d8277106465977efbebab
Steven Hunal <stevehunal@gmail.com>
2019-07-17 20:50:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,12 @@ def countdown(number)
  new_year
 end
 
+def countdown_with_sleep(number)
+  while number > 0 do
+   puts "#{number} SECOND(S)!"
+   number -= 1
+   sleep(1)
+ end
+ new_year = "HAPPY NEW YEAR!"
+ new_year
+end
